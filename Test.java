f2b0039258ee8433ccab3704a5904251c127df50
summersnow0124 <prather@daum.net>
2025-07-06 17:48:50 +0900

    Only add pom.xml and Test.java

---
+++ Test.java
@@ -0,0 +1,47 @@
+package api;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import com.google.gson.Gson;
+
+public class Test {
+    public static void main(String[] args) {
+
+    	/*List<Map<String, Object>> dataList = new ArrayList<>();
+
+    	Map<String, Object> dataMap = new HashMap<String, Object>();
+
+    	//#1
+    	dataMap.put("cp", "PIGAICAM");
+    	dataMap.put("pastureId", "G000000752");
+    	dataMap.put("mesrDt", "20240401091545");
+
+    	dataList.add(dataMap);
+
+    	//#2
+    	dataMap.put("cp", "PIGAICAM");
+    	dataMap.put("pastureId", "G000000752");
+    	dataMap.put("mesrDt", "20240403155430");
+
+    	dataList.add(dataMap);
+
+
+    	// Gson 객체 생성
+        Gson gson = new Gson();
+
+        // 데이터 리스트를 JSON 문자열로 변환
+        String json = gson.toJson(dataList);
+
+        // JSON 문자열 출력
+        System.out.println("json : "+json);*/
+
+
+
+    }
+}
